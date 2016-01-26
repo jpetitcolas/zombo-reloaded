8f54575d7780927c699899cacea68bc41f4dc64a
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-01-26 18:53:59 +0100

    Use kicking (s)ass styling!

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
+var sassOptions = [
+    'includePaths[]=./node_modules/compass-mixins/lib/',
+].join("\n");
+
 module.exports = {
     entry: {
         zombo: [
             './js/copyright.js',
             './js/newsletterLink.js',
-            './style.css'
+            './style.scss'
         ]
     },
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel', include: __dirname + '/js' },
-            { test: /\.css$/, loader: 'style!css', include: /style\.css$/ }
+            { test: /\.scss$/, loader: 'style!css!sass?' + sassOptions, include: /style\.scss$/ }
         ]
     },
     output: {
